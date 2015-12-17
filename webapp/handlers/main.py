18c354ee5452d4522024f6f0ee32158c9bd8a5ae
johnw3d <johnw@lyric.com>
2015-12-17 07:36:14 -0800

    begin parseTree display

--- webapp/handlers/main.py
+++ webapp/handlers/main.py
@@ -23,4 +23,39 @@ def run_dev_server():
 
 @kla.route('/')
 def index():
-    return "Hello, world."
+    #
+    #  test parseTree
+    from parser.parse import Parser
+    from parser.patterns.clientLog import patternDef
+    import os
+    #
+    p = Parser(os.path.expanduser("~/Dropbox/Documents/Kontiki 2014/debugging/Lloyds/pDPTu3_AT2k1-20151125-204429/error2.log"), patternDef)
+    p.parse()
+    #
+    def _renderParseTree(tree):
+        if isinstance(tree, dict):
+            nodelist = []
+            for k in sorted(tree.keys()):
+                v = tree[k]
+                if isinstance(v, (dict, list)):
+                    nodelist.append(dict(text=k, children=_renderParseTree(v)))
+                else:
+                    nodelist.append(
+
+
+    return render_template("index.html")
+
+
+#     'core' : {
+#         'data' : [
+#             { "text" : "Root node", "children" : [
+#                 { "text" : "Child node 1" },
+#                 { "text" : "Child node 2" },
+#
+#                 { "text" : "Child node 3" }
+#
+#             ]
+#             },
+#         ]
+#     }
+# });
